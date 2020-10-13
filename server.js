509f754dd8fb5db8bccc76791e36c39d45b41170
Sukhjinder Singh Warring <sukhjinder.warring@stud.th-deg.de>
2020-10-13 14:46:38 +0200

    git Ignore file added

--- server.js
+++ server.js
@@ -21,43 +21,17 @@ app.post('/search', async (req ,res) =>{
   res.send(req.body.q)
  try {
   const post = new Post();
-  //post.title = req.body.title;
   post.content = req.body.q;
    await post.save();
    res.send (post)
 } catch ( error) {
   res.status(500)
 }
-console.log(" trying to save ")
 console.log("output: "  +  req.body.q)
 res.end()}
  )
-//app.get ("/search", async(req, res) =>{
-  //try {
-    //const search = await Post.find({})
-    //res.send(posts)
-  //} catch (error){
-    //res.status(500)
- //}
-//})
-  
-// for add anything in the database 
 
-//app.post("/search", async (req , res) => {
- //res.send(req.body)
- //try {
-  //const post = new Post();
-  //post.title = req.body.title;
-  //post.content = req.body.q;
-   //await post.save();
-   //res.send (post)
-//} catch ( error) {
- // res.status(500)
-//}
-//})
-
-
-app.listen(port , function(){
+ app.listen(port , function(){
     console.log(" server is running   port 3005")
 })
 
